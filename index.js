6ddd9c1e212539babb584fdf3f1b7c3d495ef240
Daman304 <damanpreetsingh1979@gmail.com>
2022-09-28 21:49:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+var path = require('path');
+const nodemailer = require('nodemailer');
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+app.post('/', (req, res) => {
+  var transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    auth: {
+      user: 'codingdaman2006@outlook.com',
+      pass: 'daman19791983'
+    }
+  });
+  var mailOptions = {
+    from: 'codingdaman2006@outlook.com',
+    to: 'damanpreetsingh1979@gmail.com',  
+    subject: `New Feedback received`,
+    text: `Answers: ${req.body.name}; ${req.body.email}; ${req.body.feedback}`
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('<h1 class="text-center">Thank You for your feedback!</h1>');
+    }
+  });
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
